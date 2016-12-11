33e602fd34b5d0b78ef2c0c044ac6007ac501344
noble-dev <apark912x+github@gmail.com>
2016-12-10 18:08:27 -0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -26,7 +26,7 @@ bot.on('ready', ()=>{
 bot.on('message', message=>{
 	try{
 	///// PERMS CHECKS //////
-	if(message.guild.member(message.author).roles.exists('name', 'Guest') && message.content.toLowerCase().includes("baka")){
+	if(message.guild.member(message.author).roles.exists('name', 'Captain') && message.content.toLowerCase().includes("baka")){
 	 	message.delete();
 		message.channel.sendMessage('JOIN MALICIOUS INTENT');
 		console.log('what');
